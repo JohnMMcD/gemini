5a059e10010f90945dab57800fd733b5388431f5
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-05 19:54:10 -0400

    Removed old test cases; each testcase is table-driven. Also consolidated methods (removing separate buy and sell steps). Improved error messages from assertions.

--- features/positive.feature
+++ features/positive.feature
@@ -3,37 +3,35 @@
 # with BDD, or https://cucumber.io/docs/gherkin/reference/ for more
 # info on Gherkin syntax
 
+# WHAT ABOUT MARKET ORDERS?
+# The API doesn't directly support market orders because they provide
+# you with no price protection.
+# Instead, use the “immediate-or-cancel” order execution option, coupled
+# with an aggressive limit price (i.e. very high for a buy order or very
+# low for a sell order), to achieve the same result.
+
 Feature: Positive Testcases (those that should succeed)
 
-  Scenario: Run a simple test
-    Given we have enough funds
-    When we buy 5.0 BTCUSD
-    Then the order will succeed
-
-    Given we have enough funds
-    When we buy 5.0 ETHUSD
-    Then the order will succeed
-
-  Scenario: Table-driven exchange-limit maker-or-cancel buy order
+  Scenario: Table-driven exchange-limit immediate-or-cancel buy order
     Given we have enough funds
 	When we buy currency from this table
-	| amount | currency | price |
-	|    0.1 |   BTCUsd | 30000 |
-	|    1.1 |   ethUSD |   900 |
-    Then the order will succeed
+	| amount | currency |  price |
+	|    0.1 |   BTCUsd | 999999 |
+	|    0.1 |   ethUSD | 999999 |
+    Then the order will not be cancelled
 
-  Scenario: Table-driven exchange-limit maker-or-cancel sell order
+  Scenario: Table-driven exchange-limit immediate-or-cancel sell order
     Given we have enough funds
 	When we sell currency from this table
-	| amount | currency | price |
-	|    0.1 |   BTCUsd |  9000 |
-	|    1.1 |   ethUSD |   250 |
-    Then the order will succeed
+	| amount | currency |  price |
+	|    0.1 |   BTCUsd |   0.01 |
+	|    0.1 |   ethUSD |   0.01 |
+    Then the order will not be cancelled
 
-  Scenario: Table-driven exchange-limit maker-or-cancel buy or sell
+  Scenario: Table-driven exchange-limit immediate-or-cancel buy or sell
     Given we have enough funds
 	When we transact currency from this table
-	| side | amount | currency | price |
-	| buy  | 0.1    | BTCUsd   | 30001 |
-	| sell | 1.1    | ethUSD   |   245 |
-    Then the order will succeed
+	|  side | amount | currency |  price |
+	|   buy |    0.1 |   BTCUsd | 99999 |
+	|  sell |    0.1 |   ethUSD |    0.1 |
+    Then the order will not be cancelled
--- features/steps/positive_steps.py
+++ features/steps/positive_steps.py
@@ -4,79 +4,54 @@ import gemini
 # Use the regular expression matcher
 use_step_matcher('re')
 
+
 @given('we have enough funds')
 def step_impl(context):
     pass
 
-@when(f'we buy (?P<amount>\d+\.\d+) (?P<currency>(?i){gemini.SUPPORTED_SYMBOLS_REGEX})')
-def step_impl(context, amount, currency):
-    price = "3663"
-    print(f"Buying {amount} {currency} at {price}")
-    context.response = gemini.transact(amount, currency, price, "buy",
-                                       "exchange limit", ["maker-or-cancel"])
-    context.price = price
-    print(f"Price is {context.price}")
-    assert context.failed is False
+
+@when('we (?P<order_side>buy|sell) currency from this table')
+def step_impl(context, order_side):
+    for row in context.table:
+        print(f"{order_side}ing {row['amount']} {row['currency']} at {row['price']} ")
+        context.response = gemini.transact(row['amount'], row['currency'], row['price'],
+                                           f"{order_side}", "exchange limit",
+                                           ["immediate-or-cancel"])
+        context.price = row['price']
+        print(context.response)
 
 
 @when('we transact currency from this table')
 def step_impl(context):
     for row in context.table:
         price = row['price']
+        context.price = price
         side = row['side'].lower()
         currency = row['currency'].lower()
-        print(f"{side}ing from table {row['amount']} {currency} {price} ")
-        context.response = gemini.transact(row['amount'], currency, price, side, "exchange limit", ["maker-or-cancel"] )
-        context.price = price
-        print(f"Price is {context.price}")
-    assert context.failed is False
+        print(f"{side}ing from table {row['amount']} {currency} {price} in transaction")
+        context.response = gemini.transact(row['amount'], currency, price, side,
+                                           "exchange limit", ["immediate-or-cancel"])
 
-
-@when('we buy currency from this table')
+@then('the order will not be cancelled')
 def step_impl(context):
-    order_side = "buy"
-    for row in context.table:
-        print(f"{order_side}ing from table {row['amount']} {row['currency']} {row['price']} ")
-        context.response = gemini.transact(row['amount'], row['currency'], row['price'], f"{order_side}", "exchange limit", ["maker-or-cancel"] )
-        context.price = row['price']
-        print(f"Price is {context.price}")
-    assert context.failed is False
-
-
-@when('we sell currency from this table')
-def step_impl(context):
-    order_side = "sell"
-    for row in context.table:
-        print(f"{order_side}ing from table {row['amount']} {row['currency']} at {row['price']} ")
-        context.response = gemini.transact(row['amount'], row['currency'], row['price'], f"{order_side}", "exchange limit", ["maker-or-cancel"] )
-        context.price = row['price']
-        print(f"Price is {context.price}")
-        print(context.response)
-    assert context.failed is False
-
-
-@then('the order will succeed')
-def step_impl(context):
-    # TODO: Verify that is_cancelled is the correct key to examine
-    is_cancelled = context.response["is_cancelled"]
-    if is_cancelled:
-        print(f"This was cancelled because {context.response['reason']}")
-        # print(context.response["reason"])
+    if "result" in context.response:
+        if context.response["result"] == "error":
+            assert False, f"Unexpected error because of {context.response['reason']}"
+    if context.response["is_cancelled"]:
+        assert False, (f"This should not have been cancelled, but it was"
+                       f" because {context.response['reason']}")
     else:
         print("This went through!")
-    assert context.failed is is_cancelled
 
 
-"""
-The @when below is failing to match, and I don't know why. I've 
-tried several regexs and the captured grouping was not 
-recognized in each case. Setting aside for now.
-
-@when('we (?P<order_side>(buy|sell)) currency from this table')
+@then('the order will be cancelled')
 def step_impl(context):
-    for row in context.table:
-        print(f"{order_side}ing from table {row['amount']} {row['currency']} {row['price']} ")
-        context.response = gemini.transact(row['amount'], row['currency'], row['price'], f"{order_side}", "exchange limit", ["maker-or-cancel"] )
-    assert context.failed is False
-
-"""
+    if "result" in context.response:
+        if context.response["result"] == "error":
+            assert False, (f"This was supposed to be cancelled, but it "
+                           f"threw an unexpected error instead because "
+                           f"of {context.response['reason']}")
+    if context.response["is_cancelled"]:
+        print(f"As expected, this was cancelled. Reason: {context.response['reason']}")
+    else:
+        assert False, "This went through even though it was supposed to be cancelled!"
