d09d8c4a0430ccaffe6b33313d763b06fe3a394f
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-05 16:08:22 -0400

    Started to configure logging, though it's incomplete (read: not working).

---
+++ features/environment.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def before_all(context):
+    # -- SET LOG LEVEL: behave --logging-level=ERROR ...
+    # on behave command-line or in "behave.ini".
+    context.config.setup_logging(level=logging.DEBUG,
+                                 filename="%(filename).log",
+                                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                                 datefmt='%a, %d %b %Y %H:%M:%S')
+
+    # -- ALTERNATIVE: Setup logging with a configuration file.
+    # context.config.setup_logging(configfile="behave_logging.ini")
