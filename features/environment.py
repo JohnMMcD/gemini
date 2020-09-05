28542572e5327af1211f07b0cd6f2525c113b70d
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-05 19:35:33 -0400

    Updated logging configuration (still isn't working).

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ def before_all(context):
     # -- SET LOG LEVEL: behave --logging-level=ERROR ...
     # on behave command-line or in "behave.ini".
     context.config.setup_logging(level=logging.DEBUG,
-                                 filename="%(filename).log",
+                                 filename="gemini.log",
                                  format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                                  datefmt='%a, %d %b %Y %H:%M:%S')
 
