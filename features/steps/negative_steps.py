a95f281194758f5f7130de4518c1ae767e4a2366
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-07 12:04:22 -0400

    Converted from Gemini module to O-O.

--- features/steps/negative_steps.py
+++ features/steps/negative_steps.py
@@ -1,6 +1,5 @@
 from behave import given, then, use_step_matcher
-import parse
-# import gemini
+from order import *
 
 # Switch to regular expressions
 use_step_matcher("re")
@@ -11,15 +10,7 @@ def step_impl(context):
     pass
 
 
-@then('the order will throw an unknow error')
-def step_impl(context):
-    if "error" in context.response:
-        assert True
-
-
 @then('the order will give the error (?P<expected_reason>(?i)\w+)')
 def step_impl(context, expected_reason):
-    # {'result': 'error', 'reason': 'InvalidPrice', 'message': 'Invalid price for symbol BTCUSD: 0.0001'}
-    actual_result = context.response["result"]
-    actual_reason = context.response["reason"]
-    assert actual_reason == expected_reason and actual_result == "error"
+    assert ErrorResponse(context.response).verify(expected_reason), \
+        f"Order was supposed to give an error with {expected_reason}, but did not."
--- features/steps/positive_steps.py
+++ features/steps/positive_steps.py
@@ -1,5 +1,6 @@
 from behave import *
-import gemini
+from order import *
+
 
 # Use the regular expression matcher
 use_step_matcher('re')
@@ -13,45 +14,29 @@ def step_impl(context):
 @when('we (?P<order_side>buy|sell) currency from this table')
 def step_impl(context, order_side):
     for row in context.table:
-        print(f"{order_side}ing {row['amount']} {row['currency']} at {row['price']} ")
-        context.response = gemini.transact(row['amount'], row['currency'], row['price'],
-                                           f"{order_side}", "exchange limit",
-                                           ["immediate-or-cancel"])
-        context.price = row['price']
+        context.order = ImmediateOrCancelOrder(order_side, row['amount'], row['currency'], row['price'])
+        context.response = context.order.execute()
+        # stdout is only echoed when the assertion is false
+        print(context.order.payload)
         print(context.response)
 
 
 @when('we transact currency from this table')
 def step_impl(context):
     for row in context.table:
-        price = row['price']
-        context.price = price
-        side = row['side'].lower()
-        currency = row['currency'].lower()
-        print(f"{side}ing from table {row['amount']} {currency} {price} in transaction")
-        context.response = gemini.transact(row['amount'], currency, price, side,
-                                           "exchange limit", ["immediate-or-cancel"])
+        context.order = ImmediateOrCancelOrder(row['side'], row['amount'], row['currency'], row['price'])
+        context.response = context.order.execute()
+        print(context.order)
+        print(context.response)
+
 
 @then('the order will not be cancelled')
 def step_impl(context):
-    if "result" in context.response:
-        if context.response["result"] == "error":
-            assert False, f"Unexpected error because of {context.response['reason']}"
-    if context.response["is_cancelled"]:
-        assert False, (f"This should not have been cancelled, but it was"
-                       f" because {context.response['reason']}")
-    else:
-        print("This went through!")
+    assert ExecutedInFullResponse(context.response).verify(context.order), \
+        "Order should have been executed in full but wasn't."
 
 
 @then('the order will be cancelled')
 def step_impl(context):
-    if "result" in context.response:
-        if context.response["result"] == "error":
-            assert False, (f"This was supposed to be cancelled, but it "
-                           f"threw an unexpected error instead because "
-                           f"of {context.response['reason']}")
-    if context.response["is_cancelled"]:
-        print(f"As expected, this was cancelled. Reason: {context.response['reason']}")
-    else:
-        assert False, "This went through even though it was supposed to be cancelled!"
+    assert CancelledInFullResponse(context.response).verify(context.order),\
+        "Order should have been cancelled but wasn't."
