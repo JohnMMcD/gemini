73bed56f99f35367e49336f125e53c0c2f1507ca
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-08 15:28:45 -0400

    Added new testcase files and noted how to increase verbosity.

--- run_oo_all.cmd
+++ run_oo_all.cmd
@@ -1,8 +1,12 @@
-
-@REM Order matters here - you don't want to create open orders and then cross them 
+@REM Order matters here - you don't want to create open orders and then cross them
 @REM with fill or kill orders. So we do the limit orders and maker orders later.
-python -m unittest test_fill_or_kill_oo
-python -m unittest test_http_error_codes_oo
-python -m unittest test_immediate_or_cancel_oo
-python -m unittest test_stop_limit_oo
-python -m unittest test_maker_or_cancel_oo
+
+@REM Add -v for more verbosity
+@set PYUnit=python -m unittest
+%PYUnit% test_fill_or_kill_oo
+%PYUnit% test_http_error_codes_oo
+%PYUnit% test_immediate_or_cancel_oo
+%PYUnit% test_stop_limit_oo
+%PYUnit% test_maker_or_cancel_oo
+%PYUnit% test_auction_only_oo
+%PYUnit% test_indication_of_interest_oo
--- run_oo_all.sh
+++ run_oo_all.sh
@@ -1,11 +1,14 @@
 #!/bin/env bash
 
-#Order matters here - you don't want to create open orders and then cross them 
+# Order matters here - you don't want to create open orders and then cross them
 # with fill or kill orders. So we do the limit orders and maker orders later.
 
+# Add -v for more verbosity
 PYUnit="python3 -m unittest"
 ${PYUnit} test_fill_or_kill_oo
 ${PYUnit} test_http_error_codes_oo
 ${PYUnit} test_immediate_or_cancel_oo
 ${PYUnit} test_stop_limit_oo
 ${PYUnit} test_maker_or_cancel_oo
+${PYUnit} test_auction_only_oo
+${PYUnit} test_indication_of_interest_oo
