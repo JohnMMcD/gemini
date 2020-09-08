e71ea22c9e2d9e23f9f3cdc833f23bc682a7496c
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-08 13:54:56 -0400

    Adjusted for PEP 8 compliance.

--- test_fill_or_kill_oo.py
+++ test_fill_or_kill_oo.py
@@ -14,65 +14,62 @@ class TestFillOrKillOO(unittest.TestCase):
     amount_slightly_too_high = "9999"
     logger = logging.getLogger(__name__)
 
-    def testFillOrKillBuyWithHighPrice(self):
+    def test_buy_with_high_price(self):
         """Verify that your basic buy is executed in full."""
         order = FillOrKillOrder("buy", self.amount, self.symbol, self.buy_price)
         ExecutedInFullResponse(order.execute()).verify(order)
 
-    def testFillOrKillSellWithLowPrice(self):
+    def test_sell_with_low_price(self):
         """Verify that your basic sell is executed in full."""
         order = FillOrKillOrder("sell", self.amount, self.symbol, self.sell_price)
         ExecutedInFullResponse(order.execute()).verify(order)
 
-    def testFillOrKillBuyWithLowPrice(self):
+    def test_buy_with_low_price(self):
         """Verify that buying with a price that's too low gets cancelled."""
         order = FillOrKillOrder("buy", self.amount, self.symbol, self.sell_price)
         CancelledInFullResponse(order.execute()).verify(order, reason="FillOrKillWouldNotFill")
 
-    def testFillOrKillSellWithHighPrice(self):
+    def test_sell_with_high_price(self):
         """Verify that selling with a price that's too high gets cancelled."""
         order = FillOrKillOrder("sell", self.amount, self.symbol, self.buy_price)
         CancelledInFullResponse(order.execute()).verify(order, reason="FillOrKillWouldNotFill")
 
-    def testFillOrKillBuyWayTooMuch(self):
+    def test_buy_way_too_much(self):
         """Verify that buys which are way too large to be completed throw an error."""
         order = FillOrKillOrder("buy", self.amount_way_too_high, self.symbol, self.buy_price)
         ErrorResponse(order.execute()).verify("InvalidQuantity")
 
-    def testFillOrKillSellWayTooMuch(self):
+    def test_sell_way_too_much(self):
         """Verify that sells which are way too large to be completed throw an error."""
         order = FillOrKillOrder("sell", self.amount_way_too_high, self.symbol, self.sell_price)
         ErrorResponse(order.execute()).verify("InvalidQuantity")
 
-    def testFillOrKillBuySlightlyTooMuch(self):
+    def test_buy_slightly_too_much(self):
         """Verify that buys which are slightly too large to be completed are cancelled."""
         order = FillOrKillOrder("buy", self.amount_slightly_too_high, self.symbol, self.buy_price)
         CancelledInFullResponse(order.execute()).verify(order, reason="FillOrKillWouldNotFill")
 
-    def testFillOrKillSellSlightlyTooMuch(self):
+    def test_sell_slightly_too_much(self):
         """Verify that sells which are slightly too large to be completed are cancelled."""
         order = FillOrKillOrder("sell", self.amount_slightly_too_high, self.symbol, self.sell_price)
         CancelledInFullResponse(order.execute()).verify(order, reason="FillOrKillWouldNotFill")
 
-    def testFillOrKillWithNegativePrice(self):
+    def test_with_negative_price(self):
         """Verify that sells with a negative price give an error."""
         order = FillOrKillOrder("sell", self.amount, self.symbol, "-1")
         ErrorResponse(order.execute()).verify("InvalidPrice")
 
-    def testFillOrKillWithZeroPrice(self):
+    def test_with_zero_price(self):
         """Verify that sells with a zero price give an error."""
         order = FillOrKillOrder("sell", self.amount, self.symbol, "0")
         ErrorResponse(order.execute()).verify("InvalidPrice")
-
-    def testOneLineFillOrKillWithInvalidPrice(self):
-        """This isn't really an API test, this is a test of how much I like the look of everything on one line."""
-        ErrorResponse(FillOrKillOrder("sell", self.amount, self.symbol, "-1").execute()).verify("InvalidPrice")
 
     @classmethod
     def setUpClass(cls):
         # Adapted from https://docs.python.org/3/howto/logging-cookbook.html
         verbose_format = '%(asctime)s : %(levelno)s : %(funcName)s : %(message)s'
-        logging.basicConfig(level=logging.DEBUG, filemode='w', filename="./reports/" + __name__ + ".log", format=verbose_format)
+        logging.basicConfig(level=logging.DEBUG, filemode='w',
+                            filename="./reports/" + __name__ + ".log", format=verbose_format)
 
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
@@ -82,7 +79,7 @@ class TestFillOrKillOO(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        cls.logger.info("Fini! We'll pretend we cancelled all open orders and closed the session.")
+        cls.logger.info("Finished! We'll pretend we cancelled all open orders and closed the session.")
 
 
 if __name__ == '__main__':
