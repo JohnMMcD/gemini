03e8cedf4a4b63e10d6834878c75e5d08ee861de
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-09 13:13:56 -0400

    Noted directives not taking effect.

--- features/environment.py
+++ features/environment.py
@@ -5,6 +5,8 @@ def before_all(context):
     # Based on https://behave.readthedocs.io/en/latest/api.html#some-useful-environment-ideas
     # -- SET LOG LEVEL: behave --logging-level=ERROR ...
     # on behave command-line or in "behave.ini".
+    # These directives don't seem to have the desired effect - the filename is not 
+    # created, for example. But I like the logging the way it is, so I'm leaving them in place.
     verbose_format = '%(asctime)s : %(levelno)s : %(funcName)s : %(message)s'
     context.config.setup_logging(level=logging.DEBUG,
                                  filemode='w',
