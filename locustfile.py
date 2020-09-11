b0752a27e424e090725affba6f790579da9ca1d4
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-11 17:19:44 -0400

    Tweaked comments.

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,20 @@
-from order import *
-# from response import *
+from order import FillOrKillOrder
 import time
 from locust import HttpUser, task, between
 
-index_url = "/"
-admin_url = "/wp-admin/"
-# the full url is https://s-ql6tlwtx8b8al.eu1.wpsandbox.org/page-1/ - see
-# the run_locust.cmd batch file for the URL used to pre-fill the protocol, host, and port.
-page_url = "/page-"
-
 
 class NewOrder(HttpUser):
-    wait_time = between(1, 10)
+    """
+    This is a very simple class that demonstrates how to do load testing in locust.
+    # The run_locust.cmd batch file holds the URL used to pre-fill the protocol, host, and port.
+    """
+
+    wait_time = between(1, 3)
 
     @task(1)
     def view_item(self):
 
-        for item_id in range(1, 2):
+        for item_id in range(1, 1):
             order = FillOrKillOrder("buy", "1.0", "btcusd", "16000")
             headers = order.get_headers(order.get_payload())
             self.client.post(url=f"{order.ENDPOINT}", data=None, headers=headers)
@@ -24,5 +22,7 @@ class NewOrder(HttpUser):
             time.sleep(2)
 
     def on_start(self):
+        # This would be a good place to get the credentials to run this as a multi-user test.
+        # See https://www.blazemeter.com/blog/how-to-run-locust-with-different-users for one
+        # way of doing this.
         pass
-        # self.client.post("/login", json={"username": "foo", "password": "bar"})
