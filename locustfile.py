34e6addc7693e1e01500e682996a56bd33e0620d
JohnMMcD <jmcdonne+github@gmail.com>
2020-09-11 15:41:14 -0400

    Changed to run Gemini tests.

--- locustfile.py
+++ locustfile.py
@@ -1,23 +1,27 @@
+from order import *
+# from response import *
 import time
 from locust import HttpUser, task, between
 
 index_url = "/"
 admin_url = "/wp-admin/"
+# the full url is https://s-ql6tlwtx8b8al.eu1.wpsandbox.org/page-1/ - see
+# the run_locust.cmd batch file for the URL used to pre-fill the protocol, host, and port.
 page_url = "/page-"
 
 
-class QuickstartUser(HttpUser):
+class NewOrder(HttpUser):
     wait_time = between(1, 10)
 
-    @task
-    def index_page(self):
-        self.client.get(index_url)
-
-    @task(3)
+    @task(1)
     def view_item(self):
-        for item_id in range(1, 3):
-            self.client.get(f"{page_url}{item_id}")
-            time.sleep(1)
+
+        for item_id in range(1, 2):
+            order = FillOrKillOrder("buy", "1.0", "btcusd", "16000")
+            headers = order.get_headers(order.get_payload())
+            self.client.post(url=f"{order.ENDPOINT}", data=None, headers=headers)
+
+            time.sleep(2)
 
     def on_start(self):
         pass
--- run_locust.cmd
+++ run_locust.cmd
@@ -1,4 +1,5 @@
 SET LOCUST_HOST=https://s-ql6tlwtx8b8al.eu1.wpsandbox.org
+SET LOCUST_HOST=https://api.sandbox.gemini.com
 SET LOCUST_WEB_HOST=127.0.0.1
 SET LOCUST_PORT=8089
 SET LOCUST_LOCUSTFILE=locustfile.py
